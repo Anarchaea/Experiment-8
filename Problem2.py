0177920aa141eb951fe6f5a93f47c7f1cd6e41e6
Anarchaea <47448055+Anarchaea@users.noreply.github.com>
2019-11-26 16:03:17 +0800

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+cars=pd.read_csv('cars.csv')
+carsht=cars.head()
+a=carsht.iloc[:,0:11:2]
+b=cars[cars['Model']=='Mazda RX4']
+c=cars.loc[cars['Model']=='Camaro Z28',['Model','cyl']]
+d=cars.loc[[1,28,18],['Model','cyl','gear']]
+print('A\n')
+print(a,'\n')
+print('B\n')
+print(b,'\n')
+print('C\n')
+print(c,'\n')
+print('D\n')
+print(d)
